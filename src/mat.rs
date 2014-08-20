5a6f38f54d6bf47909f3cd8b91a0614f0ccc9fc8
lukemetz <luke.s.metz@gmail.com>
2014-08-20 19:20:41 -0400

    Initial moving around to make more modular and growable

---
+++ src/mat.rs
@@ -0,0 +1,195 @@
+use gen_ffi::*;
+use std::c_str::CString;
+use libc::c_uint;
+use std::fmt;
+
+pub struct Mat {
+  pub raw : *mut Matf32Raw,
+  pub shape : (uint, uint)
+}
+
+
+impl Mat {
+  /// Mostly internal function to cunstruct a matrix off of
+  /// a c pointer
+  pub fn new_from_raw(raw : *mut Matf32Raw) -> Mat {
+    unsafe {
+      let shape = (arma_Mat_f32_n_rows(raw) as uint, arma_Mat_f32_n_cols(raw) as uint);
+      Mat { raw: raw, shape : shape }
+    }
+  }
+
+  /// Construct a new matrix filled with zeros
+  pub fn zeros(rows : uint, cols : uint) -> Mat {
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_zeros(rows as c_uint, cols as c_uint))
+    }
+  }
+
+  /// Construct a new matrix filled with ones
+  pub fn ones(rows : uint, cols : uint) -> Mat {
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_ones(rows as c_uint, cols as c_uint))
+    }
+  }
+
+  /// Construct a matrix with 1's on the diagonals
+  pub fn eye(rows : uint, cols : uint) -> Mat {
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_eye(rows as c_uint, cols as c_uint))
+    }
+  }
+
+  /// Construct a matrix from random values sampled from a
+  /// normal distribution centered at 0 with std of 1.
+  pub fn randn(rows : uint, cols : uint) -> Mat {
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_randn(rows as c_uint, cols as c_uint))
+    }
+  }
+
+  /// Construct a matrix with from random values sampled from a
+  /// uniform distribution of [0, 1)
+  pub fn randu(rows : uint, cols : uint) -> Mat {
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_randu(rows as c_uint, cols as c_uint))
+    }
+  }
+}
+
+// This needs to be for base type specific functions
+impl Mat {
+  /// Index the matrix by row and column
+  ///
+  /// Failure
+  ///
+  /// Fails if index is invalid.
+  pub fn at(&self, index : (uint, uint)) -> f32 {
+    let (r, c) = self.shape;
+    let (row, col) = index;
+    if row >= r || col >= c {
+      fail!(format!("Index out of bounds - shape: {} bad index: {}", self.shape, index));
+    }
+    unsafe {
+      arma_Mat_f32_at(self.raw, row as c_uint, col as c_uint) as f32
+    }
+  }
+}
+
+pub trait MatrixFuncs {
+  /// Perform traditional matrix multiplication, or a bunch of dot products.
+  ///
+  /// # Failure
+  ///
+  /// Fails if matrix sizes are incompatible
+  fn dot(&self, mat : &Self) -> Self;
+}
+
+impl MatrixFuncs for Mat {
+  fn dot(&self, mat : &Mat) -> Mat{
+    let (_, ca) = self.shape;
+    let (rb, _) = mat.shape;
+    if ca != rb {
+        fail!(format!("Cannot multiply matrices of shape: {} and {}", self.shape, mat.shape));
+    }
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_dot_Mat_f32(self.raw, mat.raw))
+    }
+  }
+}
+
+#[unsafe_destructor]
+impl Drop for Mat {
+  fn drop(&mut self) {
+    unsafe {
+      arma_Mat_f32_free(self.raw);
+    }
+  }
+}
+
+// TODO finish this implementation
+impl fmt::Show for Mat {
+  fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::FormatError> {
+    unsafe {
+      let c_str = arma_Mat_f32_print(self.raw);
+      let string = String::from_str(CString::new(c_str, false).as_str().unwrap());
+      write!(formatter, "Mat{{shape:({}, {})\n {}\n}}", 0i, 0i, string)
+    }
+  }
+}
+
+#[cfg(test)]
+mod test_mat {
+  use super::{Mat};
+
+  #[test]
+  fn zeros() {
+    let zeros = Mat::zeros(10, 20);
+    assert_eq!(0f32, zeros.at((0,0)));
+    assert_eq!(0f32, zeros.at((9,15)));
+  }
+
+  #[test]
+  fn ones() {
+    let ones = Mat::ones(10, 20);
+    assert_eq!(1f32, ones.at((0,0)));
+    assert_eq!(1f32, ones.at((9,15)));
+  }
+
+  #[test]
+  fn eye() {
+    let eye = Mat::eye(10, 10);
+    assert_eq!(1f32, eye.at((0,0)));
+    assert_eq!(1f32, eye.at((3,3)));
+    assert_eq!(0f32, eye.at((9,3)));
+  }
+
+  #[test]
+  fn rands() {
+    let randu = Mat::randu(10, 10);
+    let randn = Mat::randn(10, 10);
+    // Spot check, no understanding of distribution
+    assert!(randn.at((0,0)) != randu.at((0,0)));
+    assert!(randu.at((2,1)) != randu.at((2,0)));
+    assert!(randn.at((2,1)) != randn.at((2,0)));
+    assert!(randn.at((3,2)) != randn.at((7,3)));
+    assert!(randu.at((3,2)) != randu.at((7,3)));
+  }
+}
+
+
+#[cfg(test)]
+mod test_mat_funcs {
+  use super::{Mat, MatrixFuncs};
+
+  #[test]
+  fn mat_dot() {
+    let eye = Mat::eye(3, 5) * 3.0f32;
+    let other = Mat::ones(5, 1);
+    let result = eye.dot(&other);
+    assert_eq!(result.shape, (3u, 1u));
+    assert_eq!(result.at((0,0)), 3.0f32);
+  }
+
+  #[test]
+  #[should_fail]
+  fn mat_dot_invalid_sizes() {
+    let eye = Mat::eye(3, 5) * 3.0f32;
+    let other = Mat::ones(6, 1);
+    let _ = eye.dot(&other);
+  }
+
+  #[test]
+  #[should_fail]
+  fn mat_at_col_out_of_bounds() {
+    let mat = Mat::ones(4, 9);
+    let _ = mat.at((2, 10));
+  }
+
+  #[test]
+  #[should_fail]
+  fn mat_at_row_out_of_bounds() {
+    let mat = Mat::ones(4, 9);
+    let _ = mat.at((7, 2));
+  }
+}
---
+++ src/matrixMath.rs
@@ -0,0 +1,177 @@
+use gen_ffi::*;
+use mat::Mat;
+
+/// Trait to enable operation overloading with Mat
+pub trait OpsWithMatrix{
+  fn do_add(&self, mat : &Mat) -> Mat;
+  fn do_sub(&self, mat : &Mat) -> Mat;
+  fn do_mul(&self, mat : &Mat) -> Mat;
+  fn do_div(&self, mat : &Mat) -> Mat;
+}
+
+impl OpsWithMatrix for f32 {
+  fn do_add(&self, mat : &Mat) -> Mat{
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_add_f32(mat.raw, *self))
+    }
+  }
+
+  fn do_sub(&self, mat : &Mat) -> Mat{
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_sub_f32(mat.raw, *self))
+    }
+  }
+
+  fn do_mul(&self, mat : &Mat) -> Mat{
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_mul_f32(mat.raw, *self))
+    }
+  }
+
+  fn do_div(&self, mat : &Mat) -> Mat{
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_div_f32(mat.raw, *self))
+    }
+  }
+}
+
+impl OpsWithMatrix for Mat {
+  fn do_add(&self, mat : &Mat) -> Mat{
+    if self.shape != mat.shape {
+      fail!(format!("Invalid matrix shapes: {}, {}", self.shape, mat.shape));
+    }
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_add_Mat_f32(mat.raw, self.raw))
+    }
+  }
+
+  fn do_sub(&self, mat : &Mat) -> Mat{
+    if self.shape != mat.shape {
+      fail!(format!("Invalid matrix shapes: {}, {}", self.shape, mat.shape));
+    }
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_sub_Mat_f32(mat.raw, self.raw))
+    }
+  }
+
+  fn do_mul(&self, mat : &Mat) -> Mat{
+    if self.shape != mat.shape {
+      fail!(format!("Invalid matrix shapes: {}, {}", self.shape, mat.shape));
+    }
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_mul_Mat_f32(mat.raw, self.raw))
+    }
+  }
+
+  fn do_div(&self, mat : &Mat) -> Mat{
+    if self.shape != mat.shape {
+      fail!(format!("Invalid matrix shapes: {}, {}", self.shape, mat.shape));
+    }
+    unsafe {
+      Mat::new_from_raw(arma_Mat_f32_div_Mat_f32(mat.raw, self.raw))
+    }
+  }
+}
+
+impl<T : OpsWithMatrix> Add<T, Mat> for Mat {
+  fn add(&self, rhs : &T) -> Mat {
+    rhs.do_add(self)
+  }
+}
+
+impl<T : OpsWithMatrix> Sub<T, Mat> for Mat {
+  fn sub(&self, rhs : &T) -> Mat {
+    rhs.do_sub(self)
+  }
+}
+
+impl<T : OpsWithMatrix> Mul<T, Mat> for Mat {
+  fn mul(&self, rhs : &T) -> Mat {
+    rhs.do_mul(self)
+  }
+}
+
+impl<T : OpsWithMatrix> Div<T, Mat> for Mat {
+  fn div(&self, rhs : &T) -> Mat {
+    rhs.do_div(self)
+  }
+}
+
+#[cfg(test)]
+mod test_ops_with_Mat {
+  use mat::Mat;
+
+  #[test]
+  fn mat_add_f32() {
+    let ones = Mat::ones(10, 20);
+    let new = ones + 1f32;
+    assert_eq!(2f32, new.at((9,15)));
+    let second = new + 1f32;
+    assert_eq!(3f32, second.at((9,15)));
+    assert_eq!(2f32, new.at((9,15)));
+  }
+
+  #[test]
+  fn mat_sub_f32() {
+    let ones = Mat::ones(10, 20);
+    let new = ones - 1f32;
+    assert_eq!(0f32, new.at((9,9)));
+  }
+
+  #[test]
+  fn mat_div_f32() {
+    let ones = Mat::ones(10, 20);
+    let new = ones / 4f32;
+    assert_eq!(0.25f32, new.at((9,9)));
+  }
+
+  #[test]
+  fn mat_mul_f32() {
+    let ones = Mat::ones(10, 20);
+    let new = ones * 4f32;
+    assert_eq!(4f32, new.at((9,9)));
+  }
+
+  #[test]
+  fn mat_add_mat() {
+    let ones = Mat::ones(10, 20);
+    let other = Mat::ones(10, 20);
+    let new = ones + other;
+    assert_eq!(2f32, new.at((9,9)));
+  }
+
+  #[test]
+  #[should_fail]
+  fn mat_add_mat_invalid_shapes() {
+    let ones = Mat::ones(10, 20);
+    let other = Mat::ones(11, 20);
+    let new = ones + other;
+    assert_eq!(2f32, new.at((9,9)));
+  }
+
+  #[test]
+  fn mat_sub_mat() {
+    let ones = Mat::ones(10, 10);
+    let other = Mat::ones(10, 10);
+    let new = ones - other;
+    assert_eq!(0f32, new.at((9,9)));
+  }
+
+  #[test]
+  fn mat_div_mat() {
+    let ones = Mat::ones(10, 10)+1f32;
+    let other = Mat::ones(10, 10)+3f32;
+    println!("{}", ones);
+    println!("{}", other);
+    let new = ones / other;
+    assert_eq!(0.5f32, new.at((9,4)));
+  }
+
+  #[test]
+  fn mat_mul_mat() {
+    let ones = Mat::ones(10, 10) + 2f32;
+    let other = Mat::ones(10, 10) + 1f32;
+    let new = ones * other;
+    assert_eq!(6f32, new.at((9,4)));
+  }
+}
